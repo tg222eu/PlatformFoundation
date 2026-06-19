8f473722a786a880308360d9987cbb4ad4f5b5ca
thorvald <thor.gudj@gmail.com>
2026-06-19 19:00:10 +0200

    Renamed key

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "stterraformstate9482"
     container_name       = "tfstate"
-    key                  = "landing-zone.tfstate"
+    key                  = "platform-foundation.tfstate"
   }
 }
 
@@ -11,7 +11,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main"{
+resource "azurerm_resource_group" "main" {
   name      = var.resource_group_name
   location  = var.location
 }
